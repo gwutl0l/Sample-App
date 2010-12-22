f5134a48193e8acef54e10dc3049f5e901b87d89
Georges Henry <georges023@gmail.com>
2010-12-22 11:45:44 -0500

    DRY'd up with 'integration_sign_in'

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -26,7 +26,7 @@ describe "Users" do
         lambda do
           visit signup_path
           fill_in "Name",         :with => "Example User"
-          fill_in "Email",        :with => "user@example.com"
+          fill_in "Email",        :with => "emailexa123@example.com"
           fill_in "password",     :with => "foobar"
           fill_in "Confirmation", :with => "foobar"
           click_button
@@ -52,10 +52,7 @@ describe "Users" do
       describe "success" do
         it "should sign a user in and out" do
           user = Factory(:user)
-          visit signin_path
-          fill_in :email,    :with => user.email
-          fill_in :password, :with => user.password
-          click_button
+          integration_sign_in(user)
           controller.should be_signed_in
           click_link "Sign out"
           controller.should_not be_signed_in
